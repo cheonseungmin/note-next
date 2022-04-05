e2a5a9b5845ee8ba098694295a45931c2a10d5c6
천승민 <cheon.sm@gsretail.com>
2022-04-05 15:54:45 +0900

    by add rewrites options to next.js, can solve CORS problem

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
+const withPlugins = require("next-compose-plugins");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    switch (process.env.NODE_ENV) {
+      case "development":
+        return [
+          {
+            source: "/:path*",
+            destination: "https://development/:path*",
+          },
+        ];
+      case "production":
+        return [
+          {
+            source: "/:path*",
+            destination: "https://production/:path*",
+          },
+        ];
+      default:
+        return [
+          {
+            source: "/:path*",
+            destination: "http://localhost:8080/:path*",
+          },
+        ];
+    }
+  },
+};
 
-module.exports = nextConfig
+module.exports = withPlugins([nextConfig]);
